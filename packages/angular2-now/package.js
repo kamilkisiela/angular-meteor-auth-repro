3bd5b0ec3f83143c045a8d80a33190c83a13bdbe
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-02-13 18:44:20 +0100

    add angular2-now

---
+++ packages/angular2-now/package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  name: 'pbastowski:angular2-now',
+  version: '1.1.6',
+  summary: 'Angular 2 @Component syntax for Meteor 1.2 and AngularJS',
+  git: 'https://github.com/pbastowski/angular2-now.git',
+  documentation: 'README.md'
+});
+
+Npm.depends({
+  'angular2-now': '1.1.6'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.1');
+  api.use('angular@1.3.4', 'client');
+  api.imply('angular@1.3.4', 'client');
+
+  api.addFiles([
+    '.npm/package/node_modules/angular2-now/dist/angular2-now.js',
+    'src/exports.js'
+  ], 'client', {
+    transpile: false
+  });
+
+  api.export(['angular2now']);
+});
